a3dd1e2b44a256a3b47c2b2c71160b5570b7d4d3
marcoscameloxavier <marcoscamelo20@gmail.com>
2024-04-19 17:00:42 -0300

    Alteração no card e tratamento de erros com buscas

--- js/index.js
+++ js/index.js
@@ -11,37 +11,53 @@ async function pesquisarLivros() {
 async function carregarLivros(query) {
     const apiKey = 'AIzaSyAzPMMGofkOJx-0Fb8uoutZV7apJKYCHqg';
     console.log("Chamou carregar livros com " + query);
-    /*const query = 'JavaScript';*/
-    const apiUrl = `https://www.googleapis.com/books/v1/volumes?q=${query}&maxResults=30&key=${apiKey}`;
+
+    if (query === "") {
+        query = "JavaScript";
+    }
+
+    const apiUrl = `https://www.googleapis.com/books/v1/volumes?q=${query}&maxResults=40&key=${apiKey}`;
     var retorno = await fetch(apiUrl);
     var dados = await retorno.json();
 
     document.getElementById("livros").innerHTML = "";
 
-    for(var i = 0; i < dados.items.length; i++) {
-        var book = dados.items[i];
-        var src = "";
+    console.log(dados);
 
-        if (book.volumeInfo.imageLinks && book.volumeInfo.imageLinks.thumbnail) {
-            src = book.volumeInfo.imageLinks.thumbnail;
-        } else {
-            src = "../img/capaDefault.png"
-        }
+    if(dados.totalItems === 0){
+        document.getElementById("livros").innerHTML = "</br> Não há resultados para sua busca!";
+    }
+    else {
+        for (var i = 0; i < dados.items.length; i++) {
+            var book = dados.items[i];
+            var src = "";
 
-        var autores = "por ";
-        if(book.volumeInfo.authors == undefined){
-            autores = "Autor não disponível.";
-        }
-        else{
-            for(var j = 0; j < book.volumeInfo.authors.length; j++){
-                autores += book.volumeInfo.authors[j] + ", ";
+            if (book.volumeInfo.imageLinks && book.volumeInfo.imageLinks.thumbnail) {
+                src = book.volumeInfo.imageLinks.thumbnail;
+            } else {
+                src = "../img/capaDefault.png"
             }
-            autores = autores.slice(0, autores.length-2);
-            autores += ".";
-        }
+
+            var autores = "por ";
+            if (book.volumeInfo.authors === undefined) {
+                autores = "Autor não disponível.";
+            } else {
+                for (var j = 0; j < book.volumeInfo.authors.length; j++) {
+                    autores += book.volumeInfo.authors[j] + ", ";
+                }
+                autores = autores.slice(0, autores.length - 2);
+                autores += ".";
+            }
+
+            var titulo = book.volumeInfo.title;
+
+            if (titulo.length > 65) {
+                titulo = titulo.slice(0, 65);
+                titulo += "...";
+            }
 
 
-        var livro = `
+            var livro = `
           <div class="card">
                <div class="card-capa">
                    <img src=${src} />
@@ -49,7 +65,7 @@ async function carregarLivros(query) {
         
                    <div class="card-corpo">
                        <div class="card-corpo-titulo">
-                           <div class="titulo-livro">${book.volumeInfo.title} </div>
+                           <div class="titulo-livro">${titulo} </div>
                            <div class="autores">${autores}</div>
                            <div class="preco-livro">R$ 45,90</div>
                        </div>
@@ -61,8 +77,17 @@ async function carregarLivros(query) {
            </div>
         `
 
-        document.getElementById("livros").innerHTML += livro;
-
+            document.getElementById("livros").innerHTML += livro;
+        }
     }
-
 }
+
+// Adiciona um ouvinte de eventos ao campo de entrada
+var input = document.getElementById("termo-pesquisa");
+input.addEventListener("keypress", function(event) {
+    // Verifica se a tecla ENTER foi pressionada
+    if (event.key === "Enter") {
+        event.preventDefault(); // Impede qualquer comportamento padrão do ENTER
+        pesquisarLivros();
+    }
+});
